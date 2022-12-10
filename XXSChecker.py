e8ee3500bc3fc32573a4f8901128551fc5de9eb6
Shayne <shayne1999.sl@gmail.com>
2022-12-10 17:44:59 +0000

    Update XXSChecker.py
    includes the ability to check for broken access control vulnerabilities.

--- XXSChecker.py
+++ XXSChecker.py
@@ -24,6 +24,15 @@ def check_sqli(url):
 
   return (False, '')
 
+def check_bac(url):
+  payloads = ['admin', 'administrator', 'root']
+  for payload in payloads:
+    r = requests.get(url, params={'username': payload, 'password': payload})
+    if 'logged in' in r.text.lower() or 'access granted' in r.text.lower():
+      return (True, payload)
+
+  return (False, '')
+
 if __name__ == '__main__':
   while True:
     url = input('Enter the URL to check (or enter "q" to quit): ')
@@ -38,5 +47,9 @@ if __name__ == '__main__':
     if sqli_result[0]:
       print('SQL injection vulnerability found! Payload:', sqli_result[1])
 
-    if not xss_result[0] and not sqli_result[0]:
+    bac_result = check_bac(url)
+    if bac_result[0]:
+      print('Broken access control vulnerability found! Payload:', bac_result[1])
+
+    if not xss_result[0] and not sqli_result[0] and not bac_result[0]:
       print('No vulnerabilities found.')
