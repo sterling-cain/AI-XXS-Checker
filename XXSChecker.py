dc650c6af7a2e2bf0a863a4b150b8d84ff24e56f
Shayne <shayne1999.sl@gmail.com>
2022-12-10 16:48:11 +0000

    Update XXSChecker.py
     includes additional information about the XSS vulnerability if one is found and includes a loop that allows the user to enter multiple URLs to check for XSS vulnerabilities.

--- XXSChecker.py
+++ XXSChecker.py
@@ -7,17 +7,45 @@ def check_xss(url):
   for payload in payloads:
     r = requests.get(url, params={'q': payload})
     if payload in r.text:
-      return True
+      return (True, payload)
 
     # Check for other indicators of XSS vulnerability
     if '<script>' in r.text.lower() or 'javascript:' in r.text.lower():
-      return True
+      return (True, '<script> or javascript:')
 
-  return False
+  return (False, '')
 
 if __name__ == '__main__':
   url = input('Enter the URL to check: ')
-  if check_xss(url):
-    print('XSS vulnerability found!')
+  result = check_xss(url)
+  if result[0]:
+    print('XSS vulnerability found! Payload:', result[1])
   else:
     print('No XSS vulnerability found.')
+import requests
+
+def check_xss(url):
+  payloads = ['<script>alert(1)</script>', '"><script>alert(1)</script>',
+              '<img src=x onerror=alert(1)>', '"><img src=x onerror=alert(1)>',
+              '<svg/onload=alert(1)>', '"><svg/onload=alert(1)>']
+  for payload in payloads:
+    r = requests.get(url, params={'q': payload})
+    if payload in r.text:
+      return (True, payload)
+
+    # Check for other indicators of XSS vulnerability
+    if '<script>' in r.text.lower() or 'javascript:' in r.text.lower():
+      return (True, '<script> or javascript:')
+
+  return (False, '')
+
+if __name__ == '__main__':
+  while True:
+    url = input('Enter the URL to check (or enter "q" to quit): ')
+    if url == 'q':
+      break
+    result = check_xss(url)
+    if result[0]:
+      print('XSS vulnerability found! Payload:', result[1])
+    else:
+      print('No XSS vulnerability found.')
